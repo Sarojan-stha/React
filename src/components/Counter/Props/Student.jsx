15bdc8cb9a23d78e295904ac67c5ea5e8e2e6169
Sarojan-stha <myselfsarojan@gmail.com>
2025-06-19 22:56:37 +0545

    prop types not worlking

--- src/components/Counter/Props/Student.jsx
+++ src/components/Counter/Props/Student.jsx
@@ -1,9 +1,17 @@
+import PropTypes from "prop-types";
+
 export default function Student(props) {
   return (
-    <>
+    <div>
       <h1>Name :{props.name}</h1>
       <h2>age: {props.age}</h2>
       <h2>Student :{props.isStudent ? "Yes" : "No"}</h2>
-    </>
+    </div>
   );
 }
+
+Student.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  isStudent: PropTypes.bool,
+};
