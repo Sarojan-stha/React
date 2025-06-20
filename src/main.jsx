b45cd81118eaf62dc17275c8c32cb20736409297
Sarojan-stha <myselfsarojan@gmail.com>
2025-06-20 16:32:06 +0545

    changed directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import StudentApp from "./Props/StudentApp";
+import StudentApp from "./components/Props/StudentApp";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
