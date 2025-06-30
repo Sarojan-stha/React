00b254f74c135e4d9aa22ae7edbec5d25fe9d90e
Sarojan-stha <myselfsarojan@gmail.com>
2025-06-30 23:36:34 +0545

    PinCode

---
+++ src/UseRef/PinCode.jsx
@@ -0,0 +1,9 @@
+export function PinCode() {
+  return (
+    <>
+      <h1>Dependency mistakes while using useEffect</h1>
+      <input placeholder="Enter the pin code" />
+      <button>Set pin code</button>
+    </>
+  );
+}
