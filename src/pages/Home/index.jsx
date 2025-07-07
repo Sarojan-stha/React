80864685d9e5571a5732578cdc1037edd0bde421
Sarojan-stha <myselfsarojan@gmail.com>
2025-07-07 19:18:33 +0545

    added useNavigate for programmatically routing

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export function Home() {
-  return <h1>This is Home page</h1>;
+  const navigate = useNavigate();
+
+  function gotoCart() {
+    navigate("/cart");
+  }
+  return (
+    <>
+      <h1>This is Home page</h1>
+      <button onClick={gotoCart}>Goto Cart</button>
+    </>
+  );
 }
