47f8b52efca61d3fdb4f0a2108c4427c095b04d5
Sarojan-stha <myselfsarojan@gmail.com>
2025-07-10 23:45:02 +0545

    nested routing page

---
+++ src/components/React-router/Items.jsx
@@ -0,0 +1,7 @@
+export function Items() {
+  return (
+    <div>
+      <h3>This is items Checkout section</h3>
+    </div>
+  );
+}
---
+++ src/components/React-router/Payement.jsx
@@ -0,0 +1,7 @@
+export function Payement() {
+  return (
+    <div>
+      <h1>This is payment section</h1>
+    </div>
+  );
+}
